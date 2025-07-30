027edb8bb7cc994595a25212b1549db5c4ff27bf
ItzKeeni <maratkanovartem89@mail.ru>
2025-07-30 20:24:54 +0300

    create site

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          background: "#ffffff",
+          foreground: "#000000",
+          main: {
+            bg: "#1B263B",
+            dark: "#0D1B2A",
+            light: "#778DA9",
+            darklight: "#415A77"
+          },
+          logo: {
+            green: "#83FF92",
+            blue: "#72A3FF",
+            red: "#FA5170"
+          }
+        }
+      }
+    }
+  }
